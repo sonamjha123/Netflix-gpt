1f6fe706e17efcd90ab96b303244ce399d3f8c36
Sonam Jha <sonamjha@Sonams-Air.lan>
2025-07-05 21:02:47 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -3,22 +3,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+    <div className = "text-2xl text-center text-white bg-gray-800 p-4">
+      
+      
           Learn React
-        </a>
-      </header>
-    </div>
+      </div>
   );
 }
 
